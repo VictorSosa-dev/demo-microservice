df699026dcb8c8a73ee7f16d6cda3952a536ef2d
VictorSosa-dev <sosavi@globalhitss.com>
2022-12-12 19:37:19 -0600

    update pom in auth-service

--- auth-service/src/main/java/com/microservice/store/auth/service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/microservice/store/auth/service/security/SecurityConfig.java
@@ -1,18 +1,13 @@
 package com.microservice.store.auth.service.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
-
-	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
-		return http.build();
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 	}
-
 }
